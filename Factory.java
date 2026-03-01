c1a3f2820e0f823de46ea72193cb2ed418ba6aef
abebed19 <abebed19@gmail.com>
2026-03-01 22:29:28 +0300

    factory method

--- Factory.java
+++ Factory.java
@@ -22,7 +22,16 @@ public class Factory {
       
         Random ticket = new Random(4);
         int number = ticket.nextInt();
-     
         
+        switch (number) {
+            case 0:
+                return new CD("Pink Floyd", "Dark Side of the Moon", 1973);
+            case 1:
+                return new CD("Wigwam", "Nuclear Nightclub", 1975);
+            case 2:
+                return new Book("Robert Martin", "Clean Code", 1);
+            default:
+                return new Book("Kent Beck", "Test Driven Development", 0.7);
+        }   
     }
 }
---
+++ Packer.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aaron
+ */
+public class Packer {
+    private Factory factory;
+    
+    public Packer(){
+        this.factory = new Factory();
+    }
+    public Box giveABoxOfThings(){
+        
+       Box box = new Box(100);
+       int i = 0 ;
+       while( i < 10){
+           Packable newThing = factory.produceNew();
+           box.add(newThing);
+           i++;
+       }
+       
+        return box;
+       
+           
+       }
+    
+    
+}
